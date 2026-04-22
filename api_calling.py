4ab4af5d5140a6fff129fbc7491710a71dfaf345
mhsahel <mhislamsahel@gmail.com>
2026-04-22 16:01:35 +0600

    Add files via upload

---
+++ api_calling.py
@@ -0,0 +1,25 @@
+from google import genai
+from  dotenv import load_dotenv
+import os
+load_dotenv()
+ 
+API_key =os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key= API_key)
+def note_generator(images):
+    prompt = "Identify the image and describe with your own ways"
+   
+    response = client.models.generate_content(
+    model="gemini-3.1-flash-lite-preview",
+    contents=[images , prompt]
+)
+    return response.text
+    
+def quize_generator(images , info):
+    prompt = f"Generate 25 quizzes based on {info}"
+    response = client.models.generate_content(
+    model="gemini-3.1-flash-lite-preview",
+    contents=[images , prompt]
+)
+    return response.text
+    
+
