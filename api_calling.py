0e9d37c827ee8af14418cc58646d0eefa5e0af25
mhsahel <mhislamsahel@gmail.com>
2026-04-23 21:50:02 +0600

    Update api_calling.py

--- api_calling.py
+++ api_calling.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_key =os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key= API_key)
 def note_generator(images):
-    prompt = "Identify the image and describe with your own ways and make notes."
+    prompt = "Identify the image and describe with  make notes and about markdown"
    
     response = client.models.generate_content(
     model="gemini-3.1-flash-lite-preview",
@@ -15,7 +15,7 @@ def note_generator(images):
     return response.text
     
 def quize_generator(images , info):
-    prompt = f"Generate 25 quizzes with answers based on {info}"
+    prompt = f"Generate 25 quizzes with answers based on {info} and avoid markdown"
     response = client.models.generate_content(
     model="gemini-3.1-flash-lite-preview",
     contents=[images , prompt]
